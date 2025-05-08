fd951460807616d5292a71f2bf4b33fd599e20d3
avinash-betha <152133994+avinash-betha@users.noreply.github.com>
2025-05-08 01:26:55 -0500

    Update setup.py seo

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='autoview',
     version='0.1.0',
-    description='One-line Python EDA assistant: auto-summary, insights, and visual report generation',
+    description='autoview: Effortless Exploratory Data Analysis with one line of code. Auto-summary, insights, and visualizations.',
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     author='Avinash Betha',
@@ -20,12 +20,16 @@ setup(
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Framework :: Jupyter',
+        'Environment :: Console',
         'Topic :: Scientific/Engineering :: Information Analysis',
         'Topic :: Utilities',
     ],
@@ -39,5 +43,6 @@ setup(
         'missingno>=0.5',
         'scikit-learn>=0.24'
     ],
+    keywords="EDA, auto EDA, data analysis, data visualization, pandas, data science, autoview, exploratory analysis",
     python_requires='>=3.6',
 )
